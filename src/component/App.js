531119add36f88c6297af18f6b2bc4de9afb9d17
Vy Hong <vhong@amazon.com>
2020-03-01 19:55:36 -0800

    Load GoogleApi
    Modified from tutorials:
    - https://gist.github.com/mikecrittenden/28fe4877ddabff65f589311fd5f8655c
    - https://blog.416serg.me/building-an-app-using-google-sheets-api-react-d69681d22ce1

--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
-import TrainingChart from './TrainingChart'
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './logo.svg';
+
+import config from "../config";
+
+import TrainingChart from './TrainingChart'
+
 
 export default class App extends Component {
+  componentDidMount() {
+    this.loadGoogleApi();
+  }
+
+  loadGoogleApi() {
+    const self = this;
+    window.gapi.load("client", function() {
+      window.gapi.client.init({
+        apiKey: config.apiKey,
+        discoveryDocs: config.discoveryDocs,
+      }).then(function () {
+        self.getSheets();
+      }, function(error) {
+        console.log(JSON.stringify(error, null, 2));
+      });
+    });
+  }
+
+  getSheets() {
+    window.gapi.client.sheets.spreadsheets.get({
+      spreadsheetId: '1nTX_iMujMwrkfvRLKw_QD08SplmZ_QscN-0CGFLfuFI',
+    }).then(function(response) {
+      console.log(response);
+      console.log(response);
+    }, function(response) {
+      console.log('Error: ' + response.result.error.message);
+    });
+  }
+
   render() {
     return (
       <div className="App">
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  apiKey: "AIzaSyBUfbxp7YVAYYDuJC_g4VDwwrZ146wKfsU",
+  discoveryDocs:
+    ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+};
