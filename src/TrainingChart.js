7731a3c1396516db3f66fbf90666dab42d8f5846
Vy Hong <vhong@amazon.com>
2020-03-01 17:45:57 -0800

    Add TrainingChart skeleton and import in App.js

---
+++ src/TrainingChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TrainingChart extends Component {
+  render() {
+    return (
+      <div className="TrainingChart">
+        Edit <code>src/TrainingChart.js</code> and save to reload.
+      </div>
+    );
+  }
+}
+
+export default TrainingChart;
